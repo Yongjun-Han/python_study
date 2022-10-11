7396480473333a03a219acee11ec53119c630c18
YongJun Han <yongjun@YongJuns-MacBook-Pro.local>
2022-10-11 23:21:15 +0900

    dictionary[]

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+pokemon_example = {
+  "id_value" : 1,
+  "name" : "이상해씨",
+  "type_value" : "풀",
+}
+# json 과 비슷하게 key 값들이 문자열로 명시됨
+
+print(pokemon_example["id_value"])
+print(pokemon_example["name"])
+print(pokemon_example["type_value"])
